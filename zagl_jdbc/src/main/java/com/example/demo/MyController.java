6833d23d03b2d5594e2ce1bf7518cd160171c213
LastGame5x5 <lobbyszablami@gmail.com>
2024-01-14 20:54:42 +0300

    version_1.3

--- zagl_jdbc/src/main/java/com/example/demo/MyController.java
+++ zagl_jdbc/src/main/java/com/example/demo/MyController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 
@@ -15,23 +16,26 @@ import java.time.LocalDate;
 public class MyController {
 
     @PostMapping("/user/insert")
-    public ResponseEntity<?> insertUser (@RequestBody User user) {
-            if (user.getLogin() == null || user.getPassword() == null || user.getEmail() == null)
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid JSON");
-            else {
-                user.setDate(LocalDate.now());
-                DbFunctions.insertUser(user);
-                System.out.println(user);
+    public ResponseEntity<?> insertUser(@RequestBody User user) {
+        if (user.getLogin() == null || user.getPassword() == null || user.getEmail() == null)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid JSON");
+        else {
+            user.setDate(LocalDate.now());
+            int rowsUpdated = DbFunctions.insertUser(user);
+            if (rowsUpdated== 0) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User with login " + user.getLogin() + " already exists");
             }
-            return ResponseEntity.ok(user);
         }
+        return ResponseEntity.ok(user);
+    }
+
     @GetMapping("/user/select/{login}")
-    public ResponseEntity<?> getSelect(@PathVariable String login){
-            User user1 = DbFunctions.getUserByLogin(login);
-            if (user1 != null) {
+    public ResponseEntity<?> getSelect(@PathVariable String login) {
+        User user1 = DbFunctions.getUserByLogin(login);
+        if (user1 != null) {
             return ResponseEntity.ok(user1);
-            } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("User not found");
-            }
+        } else {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "User not found");
         }
     }
+}
